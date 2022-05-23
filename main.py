d5fb1f0b9e17228cfe42f2503bb02ff4cdfc229d
MateusFauri <faurimateus@gmail.com>
2022-05-23 19:01:13 -0300

    Testando o BERT

--- main.py
+++ main.py
@@ -1,1 +1,76 @@
-print("Testes com o BERT")
+import torch
+import numpy as np
+import pandas as pd
+
+from tqdm.notebook import tqdm
+
+from transformers import BertTokenizer
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from transformers import BertForSequenceClassification
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+
+df = pd.read_csv('data/title_conference.csv')
+print(df)
+
+df['Conference'].value_counts()
+
+possible_labels = df.Conference.unique()
+
+
+label_dict = {}
+for index, possible_label in enumerate(possible_labels):
+    label_dict[possible_label] = index
+print(label_dict)
+
+df['label'] = df.Conference.replace(label_dict)
+
+X_train, X_val, y_train, y_val = train_test_split(df.index.values,
+                                                  df.label.values,
+                                                  test_size=0.15,
+                                                  random_state=42,
+                                                  stratify=df.label.values)
+
+df['data_type'] = ['not_set']*df.shape[0]
+
+df.loc[X_train, 'data_type'] = 'train'
+df.loc[X_val, 'data_type'] = 'val'
+
+df.groupby(['Conference', 'label', 'data_type']).count()
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',
+                                          do_lower_case=True)
+
+encoded_data_train = tokenizer.batch_encode_plus(
+    df[df.data_type == 'train'].Title.values,
+    add_special_tokens=True,
+    return_attention_mask=True,
+    pad_to_max_length=True,
+    truncation=True,
+    max_length=256,
+    return_tensors='pt'
+)
+
+encoded_data_val = tokenizer.batch_encode_plus(
+    df[df.data_type == 'val'].Title.values,
+    add_special_tokens=True,
+    return_attention_mask=True,
+    pad_to_max_length=True,
+    truncation=True,
+    max_length=256,
+    return_tensors='pt'
+)
+
+input_ids_train = encoded_data_train['input_ids']
+attention_masks_train = encoded_data_train['attention_mask']
+labels_train = torch.tensor(df[df.data_type == 'train'].label.values)
+
+input_ids_val = encoded_data_val['input_ids']
+attention_masks_val = encoded_data_val['attention_mask']
+labels_val = torch.tensor(df[df.data_type == 'val'].label.values)
+
+dataset_train = TensorDataset(input_ids_train, attention_masks_train, labels_train)
+dataset_val = TensorDataset(input_ids_val, attention_masks_val, labels_val)
